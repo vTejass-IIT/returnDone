222801a52600ca2c41af35e80496c69c2e33ea2f
Gita Disale <gdisale@hawk.iit.edu>
2023-03-31 20:14:46 -0400

    Added logos

--- client/src/components/swiperComponent/swiper.tsx
+++ client/src/components/swiperComponent/swiper.tsx
@@ -1,7 +1,29 @@
 import React, { useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay } from 'swiper';
-import puma from '../../assets/puma.jpeg';
+import Adidas_Logo from '../../assets/brand_logos/Adidas_Logo.png';
+import Aeropostale from '../../assets/brand_logos/Aeropostale.png';
+import Amazon from '../../assets/brand_logos/Amazon.png';
+import Best_Buy from '../../assets/brand_logos/Best_Buy.png';
+import CVS from '../../assets/brand_logos/CVS.png';
+import Dicks_Sporting_Goods from '../../assets/brand_logos/Dicks_Sporting_Goods.png';
+import DSW from '../../assets/brand_logos/DSW.png';
+import Foot_Locker from '../../assets/brand_logos/Foot_Locker.png';
+import Forever_21 from '../../assets/brand_logos/Forever_21.png';
+import H_M from '../../assets/brand_logos/H_M.png';
+import Home_Depot from '../../assets/brand_logos/Home_Depot.png';
+import Nike from '../../assets/brand_logos/Nike.png';
+import Nordstrom from '../../assets/brand_logos/Nordstrom.png';
+import Primark from '../../assets/brand_logos/Primark.png';
+import Puma from '../../assets/brand_logos/Puma.png';
+import Sephora from '../../assets/brand_logos/Sephora.png';
+import Target from '../../assets/brand_logos/Target.png';
+import tj_maxx from '../../assets/brand_logos/tj_maxx.png';
+import Uniqlo from '../../assets/brand_logos/Uniqlo.png';
+import walgreens from '../../assets/brand_logos/walgreens.png';
+import Walmart from '../../assets/brand_logos/Walmart.png';
+import Zara from '../../assets/brand_logos/Zara.png';
+
 // import 'swiper/swiper.scss';
 import 'swiper/css';
 import styles from './swiper.module.scss';
@@ -9,19 +31,70 @@ import styles from './swiper.module.scss';
 export default function SwiperCarousel() {
     const [brandLogos, setbrandLogos] = useState([
         {
-            logoUrl:puma
+            logoUrl:Adidas_Logo
         },
         {
-            logoUrl:puma
+            logoUrl:Aeropostale
         },
         {
-            logoUrl:puma
+            logoUrl:Amazon
         },
         {
-            logoUrl:puma
+            logoUrl:Best_Buy
         },
         {
-            logoUrl:puma
+            logoUrl:CVS
+        },
+        {
+            logoUrl:Dicks_Sporting_Goods
+        },
+        {
+            logoUrl:DSW
+        },
+        {
+            logoUrl:Foot_Locker
+        },
+        {
+            logoUrl:Forever_21
+        },
+        {
+            logoUrl:H_M
+        },
+        {
+            logoUrl:Home_Depot
+        },
+        {
+            logoUrl:Nike
+        },
+        {
+            logoUrl:Nordstrom
+        },
+        {
+            logoUrl:Primark
+        },
+        {
+            logoUrl:Puma
+        },
+        {
+            logoUrl:Sephora
+        },
+        {
+            logoUrl:Target
+        },
+        {
+            logoUrl:tj_maxx
+        },
+        {
+            logoUrl:Uniqlo
+        },
+        {
+            logoUrl:walgreens
+        },
+        {
+            logoUrl:Walmart
+        },
+        {
+            logoUrl:Zara
         }
         
     ])
@@ -44,7 +117,7 @@ export default function SwiperCarousel() {
             modules={[Autoplay]}
             > {brandLogos.map((item,index)=>(
                         <SwiperSlide key={index}>
-                            <img src={item.logoUrl}></img>
+                            <img src={item.logoUrl} width={150}></img>
                         </SwiperSlide>
                         ))}
             </Swiper>
